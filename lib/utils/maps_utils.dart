da9ed558bdac2e88864f9da132a4c3a28429ba5a
Zalezny <zaleznypl@gmail.com>
2024-05-21 18:09:59 +0200

    Added share option to point

--- lib/utils/maps_utils.dart
+++ lib/utils/maps_utils.dart
@@ -3,11 +3,16 @@ import 'package:url_launcher/url_launcher.dart';
 class MapUtils {
   MapUtils._();
 
-  static Future<void> openMap(double latitude, double longitude) async {
+  static Uri uri(double latitude, double longitude) {
     String googleUrl = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
     final uri = Uri.parse(googleUrl);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
+    return uri;
+  }
+
+  static Future<void> openMap(double latitude, double longitude) async {
+    final link = uri(latitude, longitude);
+    if (await canLaunchUrl(link)) {
+      await launchUrl(link);
     } else {
       throw 'Could not open the map.';
     }
